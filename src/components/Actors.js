22dc09201ca3116edcb374eb1c293b56f5d6240e
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-02-06 15:34:47 +0300

    Assignment done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,20 +4,20 @@ import './NavBar.css';
 
 function Actors() {
   return (
-  <div>{/*{code here}*/}
-         <h1>Actors Page</h1> 
-        {actors.map(actor => (
-          <div key={actor.name}>
-            <h2>{actor.name}</h2>
-            <ul>
-              {actor.movies.map(movie =>(
-                <li key={movie}>{movie}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
-    );
-  }
+    <div>
+      <h1>Actors Page</h1> 
+      {actors.map(({ name, movies }) => (
+        <div key={name}>
+          <h2>{name}</h2>
+          <ul>
+            {movies.map(movie => (
+              <li key={movie}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,24 +1,25 @@
 import React from "react";
-import { directors } from "../data";
-import './NavBar.css';
+import { directors } from "../data"; 
+import './NavBar.css'; 
 
 function Directors() {
-    return (
-    <div>{/*{code here}*/}
-        <h1>Directors Page</h1> 
-        {directors.map(director => (
-          <div key={director.name}>
-            <h2>{director.name}</h2>
-            <ul>
-              {director.movies.map(movie =>(
-                <li key={movie}>{movie}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
-  
-    );
-  }
+  return (
+    <div>
+      <h1>Directors Page</h1> {/* Heading for the Directors page */}
+      {/* Mapping over the directors array */}
+      {directors.map(({ name, movies }) => (
+        <div key={name}>
+          <h2>{name}</h2> {/* Displaying the director's name */}
+          <ul>
+            {/* Mapping over the movies array of the director */}
+            {movies.map(movie => (
+              <li key={movie}>{movie}</li> 
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default Directors;
